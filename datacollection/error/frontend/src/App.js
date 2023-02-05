2da25b9e146abf138975b63890b625ec059e1881
akshay25vyas <akshayvyas227@gmail.com>
2023-02-05 13:12:50 -0600

    feb-recording-screens first changes for type=standard

--- datacollection/error/frontend/src/App.js
+++ datacollection/error/frontend/src/App.js
@@ -1,110 +1,206 @@
 import React, {useState} from 'react'
 import Select from 'react-select';
 import "./App.css"
-import Iframe from 'react-iframe'
 
 function convert(arr) {
-  return arr.map((element) => {
-    return {value: element, label: element}
-  });
+    return arr.map((element) => {
+        return {value: element, label: element}
+    });
 }
 
-function CustomDropdown({arr}) {
-  console.log(convert(arr));
-  return (
-      <div>
-        <Select options={convert(arr)}/>
-      </div>
-  );
+function CustomDropdown({
+                            arr,
+                            setSelectedActivity,
+                            setSelectedPlace,
+                            setSelectedPerson,
+                            setSelectedRecordingNumber,
+                            setSelectedActivityType,
+                            menu_number
+                        }) {
+
+    const handleChange = (e) => {
+        if (menu_number === 0) {
+            setSelectedActivity(e.value);
+        }
+        if (menu_number === 1) {
+            setSelectedPlace(e.value);
+        }
+        if (menu_number === 2) {
+            setSelectedPerson(e.value);
+        }
+        if (menu_number === 3) {
+            setSelectedRecordingNumber(e.value);
+        }
+        if (menu_number === 4) {
+            setSelectedActivityType(e.value);
+        }
+    };
+    return (
+        <div>
+            <Select options={convert(arr)} onChange={handleChange}/>
+        </div>
+    );
 }
 
 
 const App = () => {
-  const recipe_name = ["r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10"];
-  const person_id = ["p1", "p2", "p3", "p4", "p5", "p6", "p7"];
-  const kitchen_id = ["k1", "k2", "k3", "k4", "k5", "k6"];
-  const recording_number = [1,2,3,4,5,6,7,8,9,10]
-  //useState
-  const getData = async () => {
-    console.log("here")
-    const getData = await fetch('http://localhost:8000/insert')
-    const showData = await getData.json()
-    console.log(showData)
+    const activity = ["COFFEE", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10"];
+    const place = ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7"];
+    const person = ["P1", "P2", "P3", "P4", "P5", "P6"];
+    const recording_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const activity_type = ["STANDARD", "ERROR"];
 
-  }
-
-
-  const generateDiv = () => {
-    //replace this with onclick upload button - use useState for setting up response
-    const res = ['S1', 'S2', 'S3', 'S4', 'S5']
-    return res.map((elem, index) => {
-      return <div key={index}>
-        <h4>{elem}</h4>
-      </div>
-    })
-  }
+    const [selectedActivity, setSelectedActivity] = useState("NO-ACTIVITY-SELECTED");
+    const [selectedPlace, setSelectedPlace] = useState("NO-PLACE-SELECTED");
+    const [selectedPerson, setSelectedPerson] = useState("NO-PERSON-SELECTED");
+    const [selectedRecordingNumber, setSelectedRecordingNumber] = useState("NO-RECORDING-NUMBER-SELECTED");
+    const [selectedActivityType, setSelectedActivityType] = useState("NO-ACTIVITY-TYPE-SELECTED");
 
 
-  return (
-      <div className='flx'>
-        <div>
-          <div>
-            <h4>Recipe name</h4>
-            <CustomDropdown arr={recipe_name}/>
-          </div>
-          <div>
-            <h4>Person Id</h4>
-            <CustomDropdown arr={person_id}/>
-          </div>
-          <div>
-            <h4>Kitchen Id</h4>
-            <CustomDropdown arr={kitchen_id}/>
-          </div>
-          <div>
-            <h4>Recording Number or time</h4>
-            <CustomDropdown arr={recording_number}/>
-          </div>
-        </div>
-        <div>
-          <div className='tc'>
-            <button onClick={() => {
-              getData()
-            }} className="button button1">Info
-            </button>
-          </div>
-          <div className="flxBtn">
+    const renderImages = () => {
+        const step_heading = [
+            'STEP - 1',
+            'STEP - 2',
+            'STEP - 3',
+            'STEP - 4',
+            'STEP - 5',
+            'STEP - 6',
+            'STEP - 7',
+            'STEP - 8',
+            'STEP - 9',
+            'STEP - 10',
+            'STEP - 11',
+            'STEP - 12',
+            'STEP - 13',
+            'STEP - 14',
+            'STEP - 15'
+        ];
+        const step_description = [
+            ' Complete description corresponding to step1 is here',
+            'STEP - 2 complete description. all details corresponding to step2 is here',
+            'STEP - 3 complete description. all details corresponding to step3 is here',
+            'STEP - 4 complete description. all details corresponding to step4 is here',
+            'STEP - 5 complete description. all details corresponding to step5 is here',
+            'STEP - 6 complete description. all details corresponding to step6 is here',
+            'STEP - 7 complete description. all details corresponding to step7 is here',
+            'STEP - 8 complete description. all details corresponding to step8 is here',
+            'STEP - 9 complete description. all details corresponding to step9 is here',
+            'STEP - 10 complete description. all details corresponding to step10 is here',
+            'STEP - 11 complete description. all details corresponding to step11 is here',
+            'STEP - 12 complete description. all details corresponding to step12 is here',
+            'STEP - 13 complete description. all details corresponding to step13 is here',
+            'STEP - 14 complete description. all details corresponding to step14 is here',
+            'STEP - 15 complete description. all details corresponding to step15 is here'
+        ];
+
+        return step_heading.map((heading_element, index) => (
             <div>
-              <button className="button button1">Green</button>
+                <div>
+                    <h2 className="step_header">{heading_element}</h2>
+                    <content className="step_header">{step_description[index]})</content>
+                </div>
+
+                <div className="flxBtn">
+                    <div>
+                        <button className="button button1">START</button>
+                    </div>
+                    <div>
+                        <button className="button button1">STOP</button>
+                    </div>
+                    <div>
+                        <button className="button button1">DELETE</button>
+                    </div>
+                </div>
+
+            </div>
+        ))
+    }
+
+
+    const generateDiv = () => {
+        //replace this with onclick upload button - use useState for setting up response
+        const res = ['S1', 'S2', 'S3', 'S4', 'S5']
+        return res.map((elem, index) => {
+            return <div key={index}>
+                <h4>{elem}</h4>
+            </div>
+        })
+    }
+
+
+    return (
+        <div className='flx'>
+            <div>
+                <div>
+                    <h4>ACTIVITY</h4>
+                    <CustomDropdown arr={activity} setSelectedActivity={setSelectedActivity} menu_number={0}/>
+                </div>
+                <div>
+                    <h4>PLACE</h4>
+                    <CustomDropdown arr={place} setSelectedPlace={setSelectedPlace} menu_number={1}/>
+                </div>
+                <div>
+                    <h4>PERSON</h4>
+                    <CustomDropdown arr={person} setSelectedPerson={setSelectedPerson} menu_number={2}/>
+                </div>
+                <div>
+                    <h4>RECORDING NUMBER</h4>
+                    <CustomDropdown arr={recording_number} setSelectedRecordingNumber={setSelectedRecordingNumber}
+                                    menu_number={3}/>
+                </div>
+                <div>
+                    <h4>TYPE</h4>
+                    <CustomDropdown arr={activity_type} setSelectedActivityType={setSelectedActivityType}
+                                    menu_number={4}/>
+                </div>
+            </div>
+            <div className="step_header">
+                <div>
+                    <div className="flxBtn">
+                        <div>
+                            <button className="button button1">DATA COLLECTION</button>
+                        </div>
+                        <div>
+                            <button className="button button1">DATA ANNOTATION</button>
+                        </div>
+                        <div>
+                            <button className="button button1">APPLICATION</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="step_header">
+                    <div className="step_header">
+                        <h1> {selectedActivity} </h1>
+                        <div className="flxBtn">
+                            <div>
+                                <button className="button button1">START</button>
+                            </div>
+                            <div>
+                                <button className="button button1">STOP</button>
+                            </div>
+                            <div>
+                                <button className="button button1">UPLOAD</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rossDiv">
+                        {renderImages()}
+                    </div>
+                </div>
             </div>
             <div>
-              <button className="button button1">Green</button>
+                <div>
+                    <h4>STEP COMPLETION STATUS</h4>
+                    {generateDiv()}
+                </div>
+                <div>
+                    <h4>UPLOAD QUEUE</h4>
+                    {generateDiv()}
+                </div>
             </div>
-            <div>
-              <button className="button button1">Green</button>
-            </div>
-          </div>
-          <div className='tc'>
-            <button className="button button1">Green</button>
-          </div>
-          <div >
-            <Iframe url="https://www.sdrive.app/embed/1ptBQD"
-                    width="640px"
-                    height="320px"
-                    id=""
-                    className=""
-                    display="block"
-                    position="relative"/>
-
-          </div>
         </div>
-        <div>
-          {generateDiv()}
-        </div>
-      </div>
-  );
+    );
 };
 
 export default App;
 
-// read step info
-// realtime video see
