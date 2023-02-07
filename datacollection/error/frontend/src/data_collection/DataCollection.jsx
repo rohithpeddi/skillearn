85485d3f32f14b65c2b4b86e129675a1e1440c29
akshay25vyas <akshayvyas227@gmail.com>
2023-02-07 12:36:10 -0600

    integrated the info api for displaying select options

--- datacollection/error/frontend/src/data_collection/DataCollection.jsx
+++ datacollection/error/frontend/src/data_collection/DataCollection.jsx
@@ -1,24 +1,14 @@
 import Box from "@mui/material/Box";
 import DataCollectionSelect from "./DataCollectionSelect";
 import DataCollectionAccordion from "./DataCollectionAccordion";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import DataCollectionGrid from "./DataCollectionGrid";
 import './DataCollection.css';
 
 const DataCollection = () => {
-    const activity = ["COFFEE", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10"];
-    const place = ["PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7"];
-    const person = ["P1", "P2", "P3", "P4", "P5", "P6"];
-    const recording_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const activity_type = ["STANDARD", "ERROR"];
-    const step_description = [' Complete description corresponding to step1 is here',
-        'Complete description corresponding to step2 is here',
-        'Complete description corresponding to step3 is here',
-        'Complete description corresponding to step4 is here',
-        'Complete description corresponding to step5 is here',
-        'Complete description corresponding to step6 is here',
-        'Complete description corresponding to step7 is here',
-        'Complete description corresponding to step8 is here'];
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const [selectedActivity, setSelectedActivity] = useState("");
     const [selectedPlace, setSelectedPlace] = useState("");
@@ -27,44 +17,83 @@ const DataCollection = () => {
     const [selectedActivityType, setSelectedActivityType] = useState("");
     const [inputIPAddress, setInputIPAddress] = useState("");
 
-    const props = {
-        selectedActivity,
-        selectedPlace,
-        selectedPerson,
-        selectedRecordingNumber,
-        selectedActivityType,
-        inputIPAddress,
-        setSelectedActivity,
-        setSelectedPlace,
-        setSelectedPerson,
-        setSelectedRecordingNumber,
-        setSelectedActivityType,
-        setInputIPAddress,
-        activity,
-        place,
-        person,
-        recording_number,
-        activity_type,
-        step_description
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/info');
+                const json = await response.json();
+                setData(json);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    else if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    else {
+        const activity = Object.keys(data.activities);
+        const place = Object.keys(data.places);
+        const person = Object.keys(data.persons);
+        const recording_number = Object.keys(data.recording_numbers)
+        const activity_type = ["STANDARD", "ERROR"];
 
-    return (
-        <Box className="data_collection">
-            <Box>
-                <DataCollectionSelect {...props} />
-            </Box>
-            <Box>
-                <DataCollectionAccordion selectedActivity={selectedActivity} step_description={step_description}
-                                         selectedType={selectedActivityType}/>
-            </Box>
-            <Box>
-                <DataCollectionGrid headerName={"STEP COMPLETION STATUS"}/>
-                <DataCollectionGrid headerName={"UPLOAD QUEUE"}/>
-            </Box>
+        const step_description = [' Complete description corresponding to step1 is here',
+            'Complete description corresponding to step2 is here',
+            'Complete description corresponding to step3 is here',
+            'Complete description corresponding to step4 is here',
+            'Complete description corresponding to step5 is here',
+            'Complete description corresponding to step6 is here',
+            'Complete description corresponding to step7 is here',
+            'Complete description corresponding to step8 is here'];
 
-        </Box>
-    );
+        const props = {
+            selectedActivity,
+            selectedPlace,
+            selectedPerson,
+            selectedRecordingNumber,
+            selectedActivityType,
+            inputIPAddress,
+            setSelectedActivity,
+            setSelectedPlace,
+            setSelectedPerson,
+            setSelectedRecordingNumber,
+            setSelectedActivityType,
+            setInputIPAddress,
+            activity,
+            place,
+            person,
+            recording_number,
+            activity_type,
+            step_description
+        };
+
+        return (
+            <Box className="data_collection">
+                <Box>
+                    <DataCollectionSelect {...props} />
+                </Box>
+                <Box>
+                    <DataCollectionAccordion selectedActivity={selectedActivity} step_description={step_description}
+                                             selectedType={selectedActivityType}/>
+                </Box>
+                <Box>
+                    <DataCollectionGrid headerName={"STEP COMPLETION STATUS"}/>
+                    <DataCollectionGrid headerName={"UPLOAD QUEUE"}/>
+                </Box>
+
+            </Box>
+        );
+    }
+
 };
 
 export default DataCollection;
