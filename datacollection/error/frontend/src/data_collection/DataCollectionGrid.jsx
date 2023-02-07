2d86fb4efeb7d0ca03794727aba6f7bb061617d8
akshay25vyas <akshayvyas227@gmail.com>
2023-02-07 07:25:06 -0600

    modified grid file css and inclusion of table

--- datacollection/error/frontend/src/data_collection/DataCollectionGrid.jsx
+++ datacollection/error/frontend/src/data_collection/DataCollectionGrid.jsx
@@ -3,31 +3,30 @@ import {Grid, Paper, styled, Typography} from "@mui/material";
 import {green} from "@mui/material/colors";
 import './DataCollectionGrid.css';
 import * as React from 'react';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
-//
-// function DenseTable() {
-//     const res = ['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5', 'Data 6', 'Data 7', 'Data 8', 'Data 9'];
-//
-//     return res.map((elem, index) => {
-//         return (
-//             <TableBody>
-//                 <TableRow
-//                     key={index}
-//                     sx={{'&:last-child td, &:last-child th': {border: 0}}}
-//                 >
-//                     <TableCell align="center">{elem}</TableCell>
-//                     <TableCell align="center">Uploading</TableCell>
-//                 </TableRow>
-//             </TableBody>
-//         );
-//     });
-// }
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+function DenseTable() {
+    const res = ['Data 1', 'Data 2', 'Data 3', 'Data 4', 'Data 5', 'Data 6', 'Data 7', 'Data 8', 'Data 9'];
+
+    return res.map((elem, index) => {
+        return (
+
+
+                <TableBody>
+                    <TableRow key={index}>
+                        <TableCell align="center">{elem}</TableCell>
+                        <TableCell align="center">Uploading</TableCell>
+                    </TableRow>
+                </TableBody>
+
+        );
+    });
+}
 
 const Item = styled(Paper)(({theme}) => ({
     backgroundColor: green,
@@ -37,7 +36,7 @@ const Item = styled(Paper)(({theme}) => ({
 
 const generateDiv = () => {
     //replace this with onclick upload button - use useState for setting up response
-    const res = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'];
+    const res = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11','S12','S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21','S22','S23', 'S24', 'S25', 'S26', 'S27', 'S28'];
     return res.map((elem, index) => {
         return (<Grid item xs={4} alignContent="center">
             <Item>{res[res.length - index - 1]}</Item>
@@ -47,7 +46,7 @@ const generateDiv = () => {
 
 const DataCollectionGrid = ({headerName}) => {
 
-    // if ({headerName} === "STEP COMPLETION STATUS") {
+    if (headerName === "STEP COMPLETION STATUS") {
         return (
             <Box className="data_collection_grid">
                 <Typography variant="h6" align="center" borderBottom="2px solid"> {headerName}</Typography>
@@ -56,21 +55,25 @@ const DataCollectionGrid = ({headerName}) => {
                 </Grid>
             </Box>
         )
-    // } else {
-    //     return (
-    //
-    //         <TableContainer component={Paper}>
-    //             <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
-    //                 <TableHead>
-    //                     <TableRow>
-    //                         <TableCell align="center">DATA TYPE</TableCell>
-    //                         <TableCell align="center">STATUS</TableCell>
-    //                     </TableRow>
-    //                 </TableHead>
-    //             </Table>
-    //             {DenseTable()}
-    //         </TableContainer>
-    //     )
-    // }
+    } else {
+        return (
+
+            <Box className="data_collection_grid">
+                <Typography variant="h6" align="center" borderBottom="2px solid"> {headerName}</Typography>
+
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">DATA TYPE</TableCell>
+                                <TableCell align="center">STATUS</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        {DenseTable()}
+                    </Table>
+                </TableContainer>
+            </Box>
+        )
+    }
 }
 export default DataCollectionGrid;
