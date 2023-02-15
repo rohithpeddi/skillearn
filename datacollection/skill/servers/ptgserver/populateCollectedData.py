c7b3bd0f471cbb6facb03ca33b11c9d18031e0ef
Rohith Peddi <rxp190007@utdallas.edu>
2023-02-15 12:31:06 -0600

    Changed directory structures

--- datacollection/skill/servers/ptgserver/populateCollectedData.py
+++
@@ -1,4 +0,0 @@
-# 1. Get all files in the directory, keep only ones with ".txt" format
-# 2. Separate lines based on "---" into lists of questions.
-# 3. For each list of questions, parse question to extract recipe step.
-# 4. For all the next lines parse line -  split at ":" make it left into a skill and right into skill note.
--- datacollection/skill/servers/readme.md
+++
@@ -1,9 +0,0 @@
-### Create a new environment for PythonDjango App
-conda create -n djangoenv python=3.8 pip
-
-conda activate djangoenv
-
-### Download MySQL
-### Run PTGSCHEMA.sql
-
-
