519fb18bf7370c4a1019792569487453a3701344
Shivvrat Arya <shivvarya@gmail.com>
2023-02-18 21:00:39 -0600

    Why: * Major Changes - Create dictionary of unique steps and their indices.
    This change addresses the need by:
    * Remove similar steps and replace them with same text
    * Create a yaml file which can be used to get the indices and steps for each recipe
    * Create a yaml file which can be used to get the indices to steps relationship
    * Create a yaml file which can be used to get the steps to indices relationship

--- datacollection/error/steps/generate_unique_steps_idx.py
+++ datacollection/error/steps/generate_unique_steps_idx.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from pprint import pformat, pprint
 
 import yaml
+from natsort import natsorted
 from tqdm import tqdm
 
 file_directory = "./"
@@ -15,11 +16,11 @@ def activity_info_text_to_dict(file_name, fileFolder=None):
     recipe_step_dict = {}
     recipe_step_dict_idx = {}
     idx = 0
-    all_steps = []
+    unique_steps = []
     with open(os.path.join(fileFolder, file_name), "r") as recipe_info_file:
         for line in recipe_info_file:
             word_list = line.split(":")
-            recipe_details = word_list[0].replace("\n", "")
+            recipe_details = word_list[0].replace("\n", "").strip()
             if len(word_list) == 1:
                 recipe_name = recipe_details
                 recipe_step_dict[recipe_name] = {}
@@ -28,32 +29,53 @@ def activity_info_text_to_dict(file_name, fileFolder=None):
                 step_details = word_list[1].replace("\n", "").strip()
                 recipe_step_dict[recipe_name][word_list[0]] = step_details
                 recipe_step_dict_idx[recipe_name][word_list[0]] = idx
-                all_steps.append(step_details)
+                unique_steps.append(step_details)
                 idx += 1
-    return recipe_step_dict, all_steps, recipe_step_dict_idx
+    print(f"Total Number of recipe steps = {idx}")
+    return recipe_step_dict, unique_steps, recipe_step_dict_idx
 
 
 activity_step_dict, all_steps, recipe_step_dict_idx = activity_info_text_to_dict(file_name, file_directory)
 print(activity_step_dict, "\n", all_steps, "\n", recipe_step_dict_idx)
 
 
-def load_similar_steps(similar_step_file):
-    # The left index is smaller than the right index
-    similar_steps = yaml.safe_load(Path(similar_step_file).read_text())
-    print(similar_steps)
-
-
-similar_steps = load_similar_steps("./same_steps.yml")
 # We will need two files - First file stores each recipe and the indices of its unique steps
 # Second file will contain a dictionary with the indices and unique steps
-def update_step_index_and_remove_steps(similar_steps, all_steps):
+def update_step_index(all_steps, activity_step_dict):
+    index_to_step = {}
+    step_to_index = {}
+    # all_steps = natsorted(list(all_steps))
+    print(f"Unique number of recipe steps - {len(all_steps)}")
     idx = 0
-    old_step_index_to_updated_step_index = {}
-    old_step_index = {}
     for k, v in enumerate(all_steps):
-        old_step_index[str(k)] = v
-    for old_index in old_step_index:
+        if str(v) in step_to_index:
+            continue
+        else:
+            step_to_index[str(v)] = idx
+            idx += 1
 
 
+    pprint(step_to_index)
+    for k, v in step_to_index.items():
+        index_to_step[str(v)] = str(k)
+    pprint(index_to_step)
+    updated_activity_dict = {}
+    for recipe in activity_step_dict:
+        updated_activity_dict[recipe] = {}
+        for step in natsorted(activity_step_dict[recipe]):
+            updated_activity_dict[recipe][step] = {}
+            updated_activity_dict[recipe][step]["text"] = activity_step_dict[recipe][step]
+            updated_activity_dict[recipe][step]["index"] = int(step_to_index[activity_step_dict[recipe][step]])
+    return updated_activity_dict, index_to_step, step_to_index
 
 
+updated_activity_dict, index_to_step, step_to_index = update_step_index(all_steps, activity_step_dict)
+pprint(updated_activity_dict)
+with open('./output/activity_dict_with_step_index.yml', 'w') as yaml_file:
+    yaml.dump(updated_activity_dict, stream=yaml_file, default_flow_style=False, sort_keys=False)
+
+with open('./output/index_to_step.yml', 'w') as yaml_file:
+    yaml.dump(index_to_step, stream=yaml_file, default_flow_style=False, sort_keys=False)
+
+with open('./output/step_to_index.yml', 'w') as yaml_file:
+    yaml.dump(step_to_index, stream=yaml_file, default_flow_style=False, sort_keys=False)
