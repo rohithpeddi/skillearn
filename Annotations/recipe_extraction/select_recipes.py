878ce57863b14fcbab52fe94bfcd600e06e4727a
Shivvrat Arya <shivvarya@gmail.com>
2023-03-10 10:51:45 -0600

    Why: * Major Changes - Add 100 recipes with maximum - 10 actions 20 actions 30 actions 40 actions 50 actions
    This change addresses the need by:
    *

--- Annotations/recipe_extraction/select_recipes.py
+++ Annotations/recipe_extraction/select_recipes.py
@@ -10,6 +10,7 @@ with open(f'./output/recipes_for_actions.pkl', 'rb') as f:
     recipes_for_actions = pickle.load(f)
 
 recipes = pd.read_csv(r'./dataset/full_dataset.csv')
+# recipes = pd.read_csv(r'./dataset/small.csv')
 recipes = recipes.rename(columns={'Unnamed: 0': "idx"})
 recipes = recipes.drop(columns=['link', 'source', 'NER'])
 # recipes = recipes.to_numpy()
@@ -23,17 +24,29 @@ def take(n, iterable):
     return list(islice(iterable, n))
 
 
-res = OrderedDict(sorted(actions_in_recipes.items(), key=lambda x: len(x[1]), reverse=True))
-first_100 = take(50, res.items())
-final_dfs = []
-for each in first_100:
-    recipe_index = each[0]
-    mask = recipes['idx'] == recipe_index
-    df_new = pd.DataFrame(recipes[mask])
-    df_new['#actions'] = len(each[1])
-    df_new = df_new.squeeze()
-    final_dfs.append(df_new)
-    print(df_new[["title", "ingredients", "directions"]].iloc[0])
-    print("Number of actions : ", len(each[1]))
-final_dfs = pd.DataFrame(final_dfs)
-final_dfs.to_csv("output/top_50_recipes.csv")
+actions_in_recipes = list(OrderedDict(sorted(actions_in_recipes.items(), key=lambda x: len(x[1]), reverse=True)).items())
+
+
+# first_100 = take(150, res.items())
+def create_df_for_max_actions(max_actions, actions_in_recipes, recipes, n=100):
+    final_dfs = []
+    num = 0
+    for each in actions_in_recipes:
+        if num == n:
+            break
+        recipe_index = each[0]
+        mask = recipes['idx'] == recipe_index
+        df_new = pd.DataFrame(recipes[mask])
+        df_new['#actions'] = len(each[1])
+        if len(each[1]) < max_actions:
+            num += 1
+            df_new = df_new.squeeze()
+            final_dfs.append(df_new)
+            print(df_new[["title", "ingredients", "directions"]].iloc[0])
+            print("Number of actions : ", len(each[1]))
+    final_dfs = pd.DataFrame(final_dfs)
+    final_dfs.to_csv(f"output/recipes_with_max_{max_actions}_actions.csv")
+
+
+for max_actions in [50, 40, 30, 20, 10]:
+    create_df_for_max_actions(max_actions, actions_in_recipes, recipes, 100)
