eb9024b31fae817f3d98ad49b9d447b5b4dd34aa
Rohith Peddi <rxp190007@utdallas.edu>
2023-04-11 02:52:24 -0500

    Changed required files for docker

--- datacollection/user_app/backend/logger_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-import os
-
-from datacollection.user_app.backend.constants import BASE_DIRECTORY
-
-log_file_path = os.path.join(BASE_DIRECTORY, "std.log")
-logging.basicConfig(filename=log_file_path, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
