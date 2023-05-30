de726ffd19377e7cfbdb949580d4dc703fa5bde8
Rohith Peddi <rxp190007@utdallas.edu>
2023-05-30 18:41:20 -0500

    Recording Review Option

--- datacollection/user_app/frontend/src/config.js
+++ datacollection/user_app/frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://192.168.1.83/api';
+const API_BASE_URL = 'http://localhost/api';
 export default API_BASE_URL;
 
 // const API_BASE_URL = 'http://cs92606.utdallas.edu/api';
