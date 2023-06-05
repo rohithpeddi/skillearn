e3cc05ea4071dfa5376040cf3e64d6f978e427bb
Rohith Peddi <rxp190007@utdallas.edu>
2023-06-05 03:03:26 -0500

    Benchmarks - 3

--- benchmarks/zero_shot_error_detection/splice_video.py
+++ benchmarks/zero_shot_error_detection/splice_video.py
@@ -1,6 +1,34 @@
+import time
+from datetime import datetime, timedelta
+
 from moviepy.editor import VideoFileClip
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import os
+
+import subprocess
+
+
+def execute_ffmpeg_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    if process.returncode != 0:
+        raise Exception(f'Error executing ffmpeg command: {command}\n{error}')
+    else:
+        return output, error
+
+
+def convert_seconds_to_ffmpeg_time(seconds):
+    delta = timedelta(seconds=seconds)
+    time = datetime(1, 1, 1) + delta
+    ffmpeg_time = time.strftime("%H:%M:%S.%f")[:-3]
+    return ffmpeg_time
+
+
+def build_ffmpeg_splice_command(input_video_path, start_time, clip_length, output_video_path):
+    start_time = convert_seconds_to_ffmpeg_time(start_time)
+    clip_length = convert_seconds_to_ffmpeg_time(clip_length)
+    command = f"ffmpeg -ss {start_time} -t {clip_length} -i {input_video_path} -c copy {output_video_path}"
+    return command
 
 
 def splice_video(video_path, output_directory):
@@ -13,12 +41,19 @@ def splice_video(video_path, output_directory):
 
     for i in range(num_intervals):
         start = i * 5
-        end = min(duration, (i + 1) * 5)
+        end = (i + 1) * 5
+
+        clip_length = min(duration-start, end - start)
+
+        print(f'Splicing {video_path} from {start} to {end}...')
 
         output_file_name = f'12_2_{i}.mp4'
         output_file_path = os.path.join(output_directory, output_file_name)
 
-        ffmpeg_extract_subclip(video_path, start, end, targetname=output_file_path)
+        command = build_ffmpeg_splice_command(video_path, start, clip_length, output_file_path)
+
+        execute_ffmpeg_command(command)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
