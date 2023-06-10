d8d7fd46151567ad5c95321c6ee4366ab2177694
Rohith Peddi <rxp190007@utdallas.edu>
2023-06-10 14:20:00 -0500

    EgoProcelData preparation

--- dataannotation/datastatistics/tests/markov_scripts.py
+++ dataannotation/datastatistics/tests/markov_scripts.py
@@ -4,9 +4,10 @@ import json
 
 
 def process_files_in_folder(source_folder):
-	recipe_id_to_name_map = {}
 	with open('recipe.json') as recipe_json_file:
 		recipe_id_to_name_map = json.load(recipe_json_file)
+	
+	videos_directory = os.path.join(source_folder, 'videos')
 	
 	# Get the list of all files in directory tree at given path
 	all_files = []
@@ -31,7 +32,7 @@ def process_files_in_folder(source_folder):
 				continue
 			
 			recipe_name = recipe_id_to_name_map[filename_parts[0]]
-			new_folder = os.path.join(source_folder, recipe_name)
+			new_folder = os.path.join(videos_directory, recipe_name)
 			
 			print("Moving {} to {}".format(file_path, new_folder))
 			# Create new folder if it doesn't exist
@@ -50,7 +51,7 @@ def transfer_files_to_normal_error_folders(source_path):
 	
 	for directory in directories:
 		print("------------------------------------------------------------------")
-		print("Directory is", directory)
+		print("Directory is ", directory)
 		# Get list of files in each directory
 		files = os.listdir(directory)
 		
@@ -152,13 +153,82 @@ def create_execution_scripts(path):
 		for file in os.listdir(path):
 			script = ""
 			if os.path.isfile(os.path.join(path, file)):
-				script += f"CUDA_VISIBLE_DEVICES=0 python -m RepLearn.TCC.main --cfg configs/{file}"
+				script += f"python -m RepLearn.TCC.main --cfg configs/{file}"
 				execution_file.write(script + "\n")
+
+
+def prepare_egoprocel_data(source_folder):
+	with open('../processed_files/activity_id_to_activity_name_map.json') as activity_id_to_activity_name_map_json_file:
+		activity_id_to_activity_name_map = json.load(activity_id_to_activity_name_map_json_file)
+	
+	# Get the list of all files in directory tree at given path
+	all_files = []
+	for dirpath, _, filenames in os.walk(source_folder):
+		for f in filenames:
+			all_files.append(os.path.join(dirpath, f))
+	
+	# Loop through all files and process them
+	for file_path in all_files:
+		if not os.path.isfile(file_path):
+			continue
+		
+		print("Processing", file_path)
+		# Split the filename on "_"
+		filename = os.path.basename(file_path)
+		filename_parts = filename.split("_")
+		
+		if len(filename_parts) > 1:
+			# Create new folder name based on the first element of the split
+			if filename_parts[0] not in activity_id_to_activity_name_map:
+				print("Recipe id {} not found in recipe.json".format(filename_parts[0]))
+				continue
 			
+			recipe_name = activity_id_to_activity_name_map[filename_parts[0]]
+			recipe_folder = os.path.join(source_folder, recipe_name)
+			
+			# Create new folder if it doesn't exist
+			os.makedirs(recipe_folder, exist_ok=True)
+			
+			# ----------------- Create Frames Folder -----------------
+			recipe_frames_folder = os.path.join(recipe_folder, 'frames')
+			os.makedirs(recipe_frames_folder, exist_ok=True)
+			
+			# ----------------- Create Annotations Folder -----------------
+			recipe_annotations_folder = os.path.join(recipe_folder, 'annotations')
+			os.makedirs(recipe_annotations_folder, exist_ok=True)
+			
+			# ----------------- Create Normal and Error Folders -----------------
+			recipe_annotations_normal_folder = os.path.join(recipe_annotations_folder, 'normal')
+			os.makedirs(recipe_annotations_normal_folder, exist_ok=True)
+			
+			recipe_annotations_error_folder = os.path.join(recipe_annotations_folder, 'error')
+			os.makedirs(recipe_annotations_error_folder, exist_ok=True)
+			
+			# ----------------- Create Videos Folder -----------------
+			
+			recipe_videos_folder = os.path.join(recipe_folder, 'videos')
+			os.makedirs(recipe_videos_folder, exist_ok=True)
+			
+			recording_number = filename_parts[1]
+			if 0 <= recording_number <= 25 or 100 <= recording_number <= 125:
+				recording_type = 'normal'
+			elif 26 <= recording_number <= 99 or recording_number > 125:
+				recording_type = 'error'
+			else:
+				continue
+			
+			recipe_videos_recording_type_folder = os.path.join(recipe_videos_folder, recording_type)
+			os.makedirs(recipe_videos_recording_type_folder, exist_ok=True)
+			
+			# Move the file to the new folder
+			new_file_path = os.path.join(recipe_videos_recording_type_folder, filename)
+			shutil.move(file_path, new_file_path)
+			print("Moved {} to {}".format(file_path, new_file_path))
+	print("Finished processing all files")
 
 
 if __name__ == '__main__':
 	# process_files_in_folder('/home/rohith/data/ptg')
 	# transfer_files_to_normal_error_folders('/home/rohith/data/ptg')
 	# rename_directories('/home/rohith/data/ptg')
-	create_directories('/home/rohith/data/ptg')
+	prepare_egoprocel_data('/home/rohith/data/ptg')
