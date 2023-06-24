d5e9d3884d1b1d24692c81a460279c1c3a851f4f
Rohith Peddi <rxp190007@utdallas.edu>
2023-06-24 11:32:25 -0500

    Synchronization and Transfer Services

--- datacollection/user_app/tests/post_processing_test.py
+++ datacollection/user_app/tests/post_processing_test.py
@@ -2,11 +2,13 @@ import os
 
 from concurrent.futures import ThreadPoolExecutor
 from datacollection.user_app.backend.app.models.recording import Recording
+from datacollection.user_app.backend.app.post_processing.nas_unzipping_service import multithreading_unzip
 from datacollection.user_app.backend.app.services.box_service import BoxService
 from datacollection.user_app.backend.app.services.firebase_service import FirebaseService
 from datacollection.user_app.backend.app.services.synchronization_service import SynchronizationServiceV2
 from datacollection.user_app.backend.app.utils.constants import Synchronization_Constants as const
 from datacollection.user_app.backend.app.utils.logger_config import get_logger, setup_logging
+
 
 setup_logging()
 logger = get_logger(__name__)
@@ -28,7 +30,7 @@ def process_directory(data_parent_directory, data_recording_directory_name, db_s
 		)
 		
 		synchronization_service.sync_streams()
-
+		
 		logger.info("-------------------------------------")
 		logger.info(f"Uploading {recording.id}")
 		logger.info("-------------------------------------")
@@ -44,33 +46,28 @@ def begin_post_processing():
 	# Create a ThreadPoolExecutor with a suitable number of threads (e.g., 4)
 	with ThreadPoolExecutor(max_workers=1) as executor:
 		for data_recording_directory_name in os.listdir(data_parent_directory):
-			executor.submit(process_directory, data_parent_directory, data_recording_directory_name, db_service, box_service)
+			executor.submit(
+				process_directory,
+				data_parent_directory,
+				data_recording_directory_name,
+				db_service,
+				box_service
+			)
+
+
+def begin_unzipping():
+	data_parent_directory = "/run/user/12345/gvfs/sftp:host=10.176.140.2/NetBackup/PTG"
+	db_service = FirebaseService()
+	recording_list = []
+	for data_recording_directory_name in os.listdir(data_parent_directory):
+		data_recording_directory_path = os.path.join(data_parent_directory, data_recording_directory_name)
+		if os.path.isdir(data_recording_directory_path):
+			recording = Recording.from_dict(db_service.fetch_recording(data_recording_directory_name))
+			recording_list.append(recording)
+	recording_list.sort(key=lambda x: x.id)
+	multithreading_unzip(recording_list, data_parent_directory)
 
 
 if __name__ == '__main__':
-	begin_post_processing()
-
-# logger.info("Preparing to upload recordings to BOX")
-# recording_list = []
-# for data_recording_directory_name in os.listdir(data_parent_directory):
-# 	data_recording_directory_path = os.path.join(data_parent_directory, data_recording_directory_name)
-# 	if os.path.isdir(data_recording_directory_path):
-# 		recording = Recording.from_dict(db_service.fetch_recording(data_recording_directory_name))
-# 		recording_list.append(recording)
-# 		logger.info(
-# 			"-------------------------------------***************************-------------------------------------")
-# 		print("=====================================")
-# 		print(f"Synchronizing {recording.id}")
-# 		print("=====================================")
-# 		synchronization_service = SynchronizationServiceV2(data_parent_directory, recording, const.BASE_STREAM,
-# 		                                                   const.SYNCHRONIZATION_STREAMS)
-# 		synchronization_service.sync_streams()
-
-# logger.info("-------------------------------------")
-# logger.info(f"Uploading {recording.id}")
-# logger.info("-------------------------------------")
-# box_service.upload_from_nas(recording, data_parent_directory)
-
-# logger.info("Uploading recordings to BOX")
-# recording_list.sort(key=lambda x: x.id)
-# box_service.upload_in_threads(recording_list, data_parent_directory)
+	#begin_post_processing()
+	begin_unzipping()
