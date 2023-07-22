12ecd41c73e15032fa5dfa39c984b55070cc6825
Rohith Peddi <rxp190007@utdallas.edu>
2023-07-22 05:56:13 -0500

    Recording Data Summarization

--- datacollection/user_app/backend/app/post_processing/recording_data_summarization_service.py
+++ datacollection/user_app/backend/app/post_processing/recording_data_summarization_service.py
@@ -7,6 +7,31 @@ from ..services.firebase_service import FirebaseService
 from ..utils.constants import Recording_Constants as const
 
 
+def get_file_size(file_path):
+	"""Gets the file size in human readable format.
+
+  Args:
+    file_path: The path to the file.
+
+  Returns:
+    The file size in human readable format.
+  """
+	
+	size = get_file_size(file_path)
+	
+	if size < 1024:
+		return str(size) + " bytes"
+	
+	elif size < 1048576:
+		return str(size // 1024) + " KB"
+	
+	elif size < 1073741824:
+		return str(size // 1048576) + " MB"
+	
+	else:
+		return str(size // 1073741824) + " GB"
+
+
 class RecordingDataSummarizationService:
 	
 	def __init__(self, recording_id, recording_data_directory, is_spatial_enabled=False):
@@ -46,12 +71,12 @@ class RecordingDataSummarizationService:
 			gopro_4k_file = os.path.join(gopro_data_directory, f"{self.recording_id}.MP4")
 			if os.path.exists(gopro_4k_file):
 				recording_summary.metadata.GOPRO_RESOLUTION_4k = True
-				recording_summary.file_sizes.GOPRO_RESOLUTION_4k = os.path.getsize(gopro_4k_file)
+				recording_summary.file_sizes.GOPRO_RESOLUTION_4k = get_file_size(gopro_4k_file)
 			
 			gopro_360p_file = os.path.join(gopro_data_directory, f"{self.recording_id}_360p.mp4")
 			if os.path.exists(gopro_360p_file):
 				recording_summary.metadata.GOPRO_RESOLUTION_360p = True
-				recording_summary.file_sizes.GOPRO_RESOLUTION_360p = os.path.getsize(gopro_360p_file)
+				recording_summary.file_sizes.GOPRO_RESOLUTION_360p = get_file_size(gopro_360p_file)
 		
 		if os.path.exists(raw_data_directory):
 			raw_depth_ahat_directory = os.path.join(raw_data_directory, const.DEPTH_AHAT)
@@ -59,19 +84,19 @@ class RecordingDataSummarizationService:
 				raw_ab_zip_file = os.path.join(raw_depth_ahat_directory, "ab.zip")
 				if os.path.exists(raw_ab_zip_file):
 					recording_summary.metadata.HOLOLENS_RAW_DEPTH_AHAT_AB_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_AHAT_AB_ZIP = os.path.getsize(raw_ab_zip_file)
+					recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_AHAT_AB_ZIP = get_file_size(raw_ab_zip_file)
 				
 				raw_depth_zip_file = os.path.join(raw_depth_ahat_directory, "depth.zip")
 				if os.path.exists(raw_depth_zip_file):
 					recording_summary.metadata.HOLOLENS_RAW_DEPTH_AHAT_DEPTH_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_AHAT_DEPTH_ZIP = os.path.getsize(raw_depth_zip_file)
+					recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_AHAT_DEPTH_ZIP = get_file_size(raw_depth_zip_file)
 				
 				if self.is_spatial_enabled:
 					raw_depth_pose_pkl_file = os.path.join(raw_depth_ahat_directory,
 					                                       f"{self.recording_id}_depth_ahat_pose.pkl")
 					if os.path.exists(raw_depth_pose_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_DEPTH_POSE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_POSE_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_RAW_DEPTH_POSE_PKL = get_file_size(
 							raw_depth_pose_pkl_file)
 			
 			raw_pv_directory = os.path.join(raw_data_directory, const.PV)
@@ -79,20 +104,20 @@ class RecordingDataSummarizationService:
 				raw_frames_zip_file = os.path.join(raw_pv_directory, "frames.zip")
 				if os.path.exists(raw_frames_zip_file):
 					recording_summary.metadata.HOLOLENS_RAW_PV_FRAMES_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_RAW_PV_FRAMES_ZIP = os.path.getsize(raw_frames_zip_file)
+					recording_summary.file_sizes.HOLOLENS_RAW_PV_FRAMES_ZIP = get_file_size(raw_frames_zip_file)
 				
 				if self.is_spatial_enabled:
 					raw_pv_pose_pkl_file = os.path.join(raw_pv_directory, f"{self.recording_id}_pv_pose.pkl")
 					if os.path.exists(raw_pv_pose_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_PV_POSE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_PV_POSE_PKL = os.path.getsize(raw_pv_pose_pkl_file)
+						recording_summary.file_sizes.HOLOLENS_RAW_PV_POSE_PKL = get_file_size(raw_pv_pose_pkl_file)
 			
 			raw_mc_directory = os.path.join(raw_data_directory, const.MC)
 			if os.path.exists(raw_mc_directory):
 				raw_mc_pkl_file = os.path.join(raw_mc_directory, f"{self.recording_id}_mc.pkl")
 				if os.path.exists(raw_mc_pkl_file):
 					recording_summary.metadata.HOLOLENS_RAW_MC_PKL = True
-					recording_summary.file_sizes.HOLOLENS_RAW_MC_PKL = os.path.getsize(raw_mc_pkl_file)
+					recording_summary.file_sizes.HOLOLENS_RAW_MC_PKL = get_file_size(raw_mc_pkl_file)
 			
 			if self.is_spatial_enabled:
 				raw_spatial_directory = os.path.join(raw_data_directory, const.SPATIAL)
@@ -100,7 +125,7 @@ class RecordingDataSummarizationService:
 					raw_spatial_pkl_file = os.path.join(raw_spatial_directory, f"{self.recording_id}_spatial.pkl")
 					if os.path.exists(raw_spatial_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_SPATIAL_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_SPATIAL_PKL = os.path.getsize(raw_spatial_pkl_file)
+						recording_summary.file_sizes.HOLOLENS_RAW_SPATIAL_PKL = get_file_size(raw_spatial_pkl_file)
 				
 				raw_imu_data_directory = os.path.join(raw_data_directory, const.IMU)
 				if os.path.exists(raw_imu_data_directory):
@@ -108,21 +133,21 @@ class RecordingDataSummarizationService:
 					                                              f"{self.recording_id}_imu_accelerometer.pkl")
 					if os.path.exists(raw_imu_accelerometer_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_IMU_ACCELEROMETER_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_IMU_ACCELEROMETER_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_RAW_IMU_ACCELEROMETER_PKL = get_file_size(
 							raw_imu_accelerometer_pkl_file)
 					
 					raw_imu_gyroscope_pkl_file = os.path.join(raw_imu_data_directory,
 					                                          f"{self.recording_id}_imu_gyroscope.pkl")
 					if os.path.exists(raw_imu_gyroscope_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_IMU_GYROSCOPE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_IMU_GYROSCOPE_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_RAW_IMU_GYROSCOPE_PKL = get_file_size(
 							raw_imu_gyroscope_pkl_file)
 					
 					raw_imu_magnetometer_pkl_file = os.path.join(raw_imu_data_directory,
 					                                             f"{self.recording_id}_imu_magnetometer.pkl")
 					if os.path.exists(raw_imu_magnetometer_pkl_file):
 						recording_summary.metadata.HOLOLENS_RAW_IMU_MAGNETOMETER_PKL = True
-						recording_summary.file_sizes.HOLOLENS_RAW_IMU_MAGNETOMETER_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_RAW_IMU_MAGNETOMETER_PKL = get_file_size(
 							raw_imu_magnetometer_pkl_file)
 		
 		sync_data_directory = os.path.join(self.recording_data_directory, const.SYNC)
@@ -132,12 +157,12 @@ class RecordingDataSummarizationService:
 				sync_ab_zip_file = os.path.join(sync_depth_ahat_directory, "ab.zip")
 				if os.path.exists(sync_ab_zip_file):
 					recording_summary.metadata.HOLOLENS_SYNC_DEPTH_AHAT_AB_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_AHAT_AB_ZIP = os.path.getsize(sync_ab_zip_file)
+					recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_AHAT_AB_ZIP = get_file_size(sync_ab_zip_file)
 				
 				sync_depth_zip_file = os.path.join(sync_depth_ahat_directory, "depth.zip")
 				if os.path.exists(sync_depth_zip_file):
 					recording_summary.metadata.HOLOLENS_SYNC_DEPTH_AHAT_DEPTH_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_AHAT_DEPTH_ZIP = os.path.getsize(
+					recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_AHAT_DEPTH_ZIP = get_file_size(
 						sync_depth_zip_file)
 				
 				if self.is_spatial_enabled:
@@ -145,7 +170,7 @@ class RecordingDataSummarizationService:
 					                                        f"{self.recording_id}_depth_ahat_pose.pkl")
 					if os.path.exists(sync_depth_pose_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_DEPTH_POSE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_POSE_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_SYNC_DEPTH_POSE_PKL = get_file_size(
 							sync_depth_pose_pkl_file)
 			
 			sync_pv_directory = os.path.join(sync_data_directory, const.PV)
@@ -153,20 +178,20 @@ class RecordingDataSummarizationService:
 				sync_frames_zip_file = os.path.join(sync_pv_directory, "frames.zip")
 				if os.path.exists(sync_frames_zip_file):
 					recording_summary.metadata.HOLOLENS_SYNC_PV_FRAMES_ZIP = True
-					recording_summary.file_sizes.HOLOLENS_SYNC_PV_FRAMES_ZIP = os.path.getsize(sync_frames_zip_file)
+					recording_summary.file_sizes.HOLOLENS_SYNC_PV_FRAMES_ZIP = get_file_size(sync_frames_zip_file)
 				
 				if self.is_spatial_enabled:
 					sync_pv_pose_pkl_file = os.path.join(sync_pv_directory, f"{self.recording_id}_pv_pose.pkl")
 					if os.path.exists(sync_pv_pose_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_PV_POSE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_PV_POSE_PKL = os.path.getsize(sync_pv_pose_pkl_file)
+						recording_summary.file_sizes.HOLOLENS_SYNC_PV_POSE_PKL = get_file_size(sync_pv_pose_pkl_file)
 			
 			sync_mc_directory = os.path.join(sync_data_directory, const.MC)
 			if os.path.exists(sync_mc_directory):
 				sync_mc_pkl_file = os.path.join(sync_mc_directory, f"{self.recording_id}_mc.pkl")
 				if os.path.exists(sync_mc_pkl_file):
 					recording_summary.metadata.HOLOLENS_SYNC_MC_PKL = True
-					recording_summary.file_sizes.HOLOLENS_SYNC_MC_PKL = os.path.getsize(sync_mc_pkl_file)
+					recording_summary.file_sizes.HOLOLENS_SYNC_MC_PKL = get_file_size(sync_mc_pkl_file)
 			
 			if self.is_spatial_enabled:
 				sync_spatial_directory = os.path.join(sync_data_directory, const.SPATIAL)
@@ -174,7 +199,7 @@ class RecordingDataSummarizationService:
 					sync_spatial_pkl_file = os.path.join(sync_spatial_directory, f"{self.recording_id}_spatial.pkl")
 					if os.path.exists(sync_spatial_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_SPATIAL_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_SPATIAL_PKL = os.path.getsize(sync_spatial_pkl_file)
+						recording_summary.file_sizes.HOLOLENS_SYNC_SPATIAL_PKL = get_file_size(sync_spatial_pkl_file)
 				
 				sync_imu_data_directory = os.path.join(sync_data_directory, const.IMU)
 				if os.path.exists(sync_imu_data_directory):
@@ -182,21 +207,21 @@ class RecordingDataSummarizationService:
 					                                               f"{self.recording_id}_imu_accelerometer.pkl")
 					if os.path.exists(sync_imu_accelerometer_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_IMU_ACCELEROMETER_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_ACCELEROMETER_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_ACCELEROMETER_PKL = get_file_size(
 							sync_imu_accelerometer_pkl_file)
 					
 					sync_imu_gyroscope_pkl_file = os.path.join(sync_imu_data_directory,
 					                                           f"{self.recording_id}_imu_gyroscope.pkl")
 					if os.path.exists(sync_imu_gyroscope_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_IMU_GYROSCOPE_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_GYROSCOPE_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_GYROSCOPE_PKL = get_file_size(
 							sync_imu_gyroscope_pkl_file)
 					
 					sync_imu_magnetometer_pkl_file = os.path.join(sync_imu_data_directory,
 					                                              f"{self.recording_id}_imu_magnetometer.pkl")
 					if os.path.exists(sync_imu_magnetometer_pkl_file):
 						recording_summary.metadata.HOLOLENS_SYNC_IMU_MAGNETOMETER_PKL = True
-						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_MAGNETOMETER_PKL = os.path.getsize(
+						recording_summary.file_sizes.HOLOLENS_SYNC_IMU_MAGNETOMETER_PKL = get_file_size(
 							sync_imu_magnetometer_pkl_file)
 		
 		self.db_service.update_recording_summary(recording_summary.to_dict())
