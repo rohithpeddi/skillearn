76b3b6004f600066bfcfd9476aa4d7bc1c45b2ed
Rohith Peddi <rxp190007@utdallas.edu>
2023-07-28 05:25:51 -0500

    Transfer from Local to NAS

--- datacollection/user_app/tests/compare_transfer.py
+++ datacollection/user_app/tests/compare_transfer.py
@@ -1,5 +1,13 @@
 import os
 import shutil
+import shutil
+import time
+
+from concurrent.futures import ThreadPoolExecutor
+from datacollection.user_app.backend.app.utils.logger_config import get_logger, setup_logging
+
+setup_logging()
+logger = get_logger(__name__)
 
 
 #
@@ -12,11 +20,11 @@ import shutil
 # 		destination_file = os.path.join(destination_root, file)
 #
 # 		if not os.path.isfile(destination_file):
-# 			print(f"File {destination_file} is missing.")
+# 			logger.info(f"File {destination_file} is missing.")
 # 			return False
 #
 # 		if os.path.getsize(source_file) != os.path.getsize(destination_file):
-# 			print(f"File {destination_file} has a different size.")
+# 			logger.info(f"File {destination_file} has a different size.")
 # 			return False
 #
 # 	return True
@@ -27,11 +35,11 @@ import shutil
 # 	are_files_matching = compare_files_in_folder(source_root, destination_root)
 #
 # 	if not are_files_matching:
-# 		print("-----------------------------------")
-# 		print("Files are missing.")
-# 		print("Source folder: ", source_root)
-# 		print("Destination folder: ", destination_root)
-# 		print("-----------------------------------")
+# 		logger.info("-----------------------------------")
+# 		logger.info("Files are missing.")
+# 		logger.info("Source folder: ", source_root)
+# 		logger.info("Destination folder: ", destination_root)
+# 		logger.info("-----------------------------------")
 # 		return False
 #
 # 	# 2. Get all the sub folders in the source folder and destination folder and compare them recursively
@@ -44,24 +52,24 @@ import shutil
 # 		destination_directory = os.path.join(destination_root, directory)
 #
 # 		if not os.path.isdir(destination_directory):
-# 			print(f"Directory {destination_directory} is missing.")
+# 			logger.info(f"Directory {destination_directory} is missing.")
 # 			return False
 #
 # 		are_folders_matching = compare_folders(source_directory, destination_directory)
 #
 # 		if not are_folders_matching:
-# 			print("-----------------------------------")
-# 			print("Folder files are missing.")
-# 			print("Source folder: ", source_directory)
-# 			print("Destination folder: ", destination_directory)
-# 			print("-----------------------------------")
+# 			logger.info("-----------------------------------")
+# 			logger.info("Folder files are missing.")
+# 			logger.info("Source folder: ", source_directory)
+# 			logger.info("Destination folder: ", destination_directory)
+# 			logger.info("-----------------------------------")
 # 			return False
 
 
 def compare_and_transfer_info_files(source_parent_directory, destination_parent_directory):
     for recording_id in os.listdir(source_parent_directory):
-        print("----------------------------------------------------------")
-        print(f"Processing recording {recording_id}")
+        logger.info("----------------------------------------------------------")
+        logger.info(f"Processing recording {recording_id}")
         source_recording_directory = os.path.join(source_parent_directory, recording_id)
         if os.listdir(source_recording_directory):
             source_recording_info_file_path = os.path.join(source_recording_directory, "Hololens2Info.dat")
@@ -69,64 +77,83 @@ def compare_and_transfer_info_files(source_parent_directory, destination_parent_
             destination_recording_info_file_path = os.path.join(destination_recording_folder, "Hololens2Info.dat")
             
             if not os.path.isdir(destination_recording_folder):
-                print("Recording folder does not exist. Skipping this recording.")
+                logger.info("Recording folder does not exist. Skipping this recording.")
                 continue
             
             if os.path.isfile(source_recording_info_file_path):
                 if not os.path.isfile(destination_recording_info_file_path):
-                    print(f"Copying file {destination_recording_info_file_path}")
+                    logger.info(f"Copying file {destination_recording_info_file_path}")
                     shutil.copyfile(source_recording_info_file_path, destination_recording_info_file_path)
                 else:
                     if os.path.getsize(source_recording_info_file_path) != os.path.getsize(
                             destination_recording_info_file_path):
-                        print(f"File {destination_recording_info_file_path} has a different size.")
-                        print("Deleting the file and copying again.")
+                        logger.info(f"File {destination_recording_info_file_path} has a different size.")
+                        logger.info("Deleting the file and copying again.")
                         shutil.rmtree(destination_recording_info_file_path)
                         shutil.copyfile(source_recording_info_file_path, destination_recording_info_file_path)
-                    print(f"File {destination_recording_info_file_path} already exists.")
+                    logger.info(f"File {destination_recording_info_file_path} already exists.")
 
 
 def check_file_size_and_transfer(source_file_path, destination_file_path):
     if os.path.isfile(source_file_path):
         if not os.path.isfile(destination_file_path):
-            print(f"Copying file {destination_file_path}")
+            logger.info(f"Copying file {destination_file_path}")
             shutil.copyfile(source_file_path, destination_file_path)
         else:
             if os.path.getsize(source_file_path) != os.path.getsize(destination_file_path):
-                print(f"File {destination_file_path} has a different size.")
-                print("Deleting the file and copying again.")
+                logger.info(f"File {destination_file_path} has a different size.")
+                logger.info("Deleting the file and copying again.")
                 shutil.rmtree(destination_file_path)
                 shutil.copyfile(source_file_path, destination_file_path)
-            print(f"File {destination_file_path} already exists.")
+            logger.info(f"File {destination_file_path} already exists.")
 
 
-def compare_and_transfer(source_parent_directory, destination_parent_directory):
-    for recording_id in os.listdir(source_parent_directory):
-        print("----------------------------------------------------------")
-        print(f"Processing recording {recording_id}")
-        source_sync_directory = os.path.join(source_parent_directory, recording_id, "sync")
-        destination_sync_directory = os.path.join(destination_parent_directory, recording_id, "sync")
-        
-        source_depth_ahat_directory = os.path.join(source_sync_directory, "depth_ahat")
-        source_ab_zip_file = os.path.join(source_depth_ahat_directory, "ab.zip")
-        source_depth_zip_file = os.path.join(source_depth_ahat_directory, "depth.zip")
-        
-        destination_depth_ahat_directory = os.path.join(destination_sync_directory, "depth_ahat")
-        destination_ab_zip_file = os.path.join(destination_depth_ahat_directory, "ab.zip")
-        destination_depth_zip_file = os.path.join(destination_depth_ahat_directory, "depth.zip")
-        
-        check_file_size_and_transfer(source_ab_zip_file, destination_ab_zip_file)
-        check_file_size_and_transfer(source_depth_zip_file, destination_depth_zip_file)
-        
-        source_pv_directory = os.path.join(source_sync_directory, "pv")
-        source_pv_zip_file = os.path.join(source_pv_directory, "frames.zip")
-        
-        destination_pv_directory = os.path.join(destination_sync_directory, "pv")
-        destination_pv_zip_file = os.path.join(destination_pv_directory, "frames.zip")
-        
-        check_file_size_and_transfer(source_pv_zip_file, destination_pv_zip_file)
+def compare_and_transfer(source_parent_directory, destination_parent_directory, recording_id):
+    logger.info("----------------------------------------------------------")
+    logger.info(f"[{recording_id}] BEGIN TRANSFER ZIPPED FILES FROM LOCAL TO NAS")
+    source_sync_directory = os.path.join(source_parent_directory, recording_id, "sync")
+    destination_sync_directory = os.path.join(destination_parent_directory, recording_id, "sync")
+    
+    source_depth_ahat_directory = os.path.join(source_sync_directory, "depth_ahat")
+    source_ab_zip_file = os.path.join(source_depth_ahat_directory, "ab.zip")
+    source_depth_zip_file = os.path.join(source_depth_ahat_directory, "depth.zip")
+    
+    destination_depth_ahat_directory = os.path.join(destination_sync_directory, "depth_ahat")
+    destination_ab_zip_file = os.path.join(destination_depth_ahat_directory, "ab.zip")
+    destination_depth_zip_file = os.path.join(destination_depth_ahat_directory, "depth.zip")
+    
+    logger.info(f"[{recording_id}  Checking file size and transferring [AB]")
+    check_file_size_and_transfer(source_ab_zip_file, destination_ab_zip_file)
+    
+    logger.info(f"[{recording_id}  Checking file size and transferring [Depth]")
+    check_file_size_and_transfer(source_depth_zip_file, destination_depth_zip_file)
+    
+    source_pv_directory = os.path.join(source_sync_directory, "pv")
+    source_pv_zip_file = os.path.join(source_pv_directory, "frames.zip")
+    
+    destination_pv_directory = os.path.join(destination_sync_directory, "pv")
+    destination_pv_zip_file = os.path.join(destination_pv_directory, "frames.zip")
+    
+    logger.info(f"[{recording_id}  Checking file size and transferring [PV]")
+    check_file_size_and_transfer(source_pv_zip_file, destination_pv_zip_file)
+    
+    logger.info(f"[{recording_id}] END TRANSFER ZIPPED FILES FROM LOCAL TO NAS")
     
     
+def multithreading_video_conversion(source_parent_directory, destination_parent_directory):
+    max_workers = 10
+    recording_ids = os.listdir(source_parent_directory)
+    logger.info("Preparing to synchronize using ThreadPoolExecutor with max_workers = 1")
+    # Create a ThreadPoolExecutor with a suitable number of threads (e.g., 4)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for index, recording_id in recording_ids:
+            executor.submit(
+                compare_and_transfer,
+                source_parent_directory,
+                destination_parent_directory,
+                recording_id
+            )
+
 
 if __name__ == '__main__':
     # source_directory = "/run/user/12345/gvfs/sftp:host=10.176.140.2/NetBackup/BACKUP_STUFF/PTG_HOLOLENS_BACKUP/hololens_bak_bharath"
